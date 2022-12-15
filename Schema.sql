6504e7679487576531493e0f2adec9862e423d07
Astoa3 <108148923+Astoa3@users.noreply.github.com>
2022-12-15 14:17:46 -0500

    Update Schema.sql

--- Schema.sql
+++ Schema.sql
@@ -10,8 +10,8 @@ CREATE TABLE TruDB.Customers (
     email varchar(255) DEFAULT NULL
 );
 #Example entries
-INSERT INTO Customers VALUE("Jon","Smith","1st Street","260-123-1234","JonSmith@gmail.com");
-INSERT INTO Customers VALUE("Mary","Winter","22 W Mountain Street","260-411-0001","MaryWinter@gmail.com");
-INSERT INTO Customers VALUE("Robin","Rider","88 River Street","260-555-9998","RobinR@gmail.com");
-INSERT INTO Customers VALUE("Tom","Adam","27 Cat Street","260-886-6688","TomAdam@gmail.com");
+INSERT INTO Customers VALUE("Jon","Smith","1st Street","111-123-1234","JonSmith@gmail.com");
+INSERT INTO Customers VALUE("Mary","Winter","22 W Mountain Street","111-411-0001","MaryWinter@gmail.com");
+INSERT INTO Customers VALUE("Robin","Rider","88 River Street","111-555-9998","RobinR@gmail.com");
+INSERT INTO Customers VALUE("Tom","Adam","27 Cat Street","111-886-6688","TomAdam@gmail.com");
 
